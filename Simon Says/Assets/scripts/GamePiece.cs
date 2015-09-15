472f2f96620ee72d840eca3b6d792e9589a127bb
caseyboy65 <caseyboy65@yahoo.com>
2015-09-14 20:06:17 -0400

    Fixed issue bug
    Fixed issue around user's last click immediately starts next round. Now
    waits for last animation to fire and then starts next round.

---
+++ Simon Says/Assets/scripts/GamePiece.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GamePiece : MonoBehaviour {
+
+	Animator animate;
+	bool isAnimating = false;
+	bool isPlayerClicking = false;
+	
+	//GameController GameController;
+	// Use this for initialization
+	void Start () {
+		GameObject.Find ("GameController").GetComponent<GameController> ().registerGamePiece(gameObject);
+		animate = (Animator) GetComponent ("Animator");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isAnimating || isPlayerClicking) {
+			Debug.Log ("Is in idle state" + animate.GetCurrentAnimatorStateInfo (0).IsName ("Idle"));
+			if (animate.GetCurrentAnimatorStateInfo (0).IsName ("Idle")) {
+			//if (animate.IsInTransition ()) {
+				if (isAnimating) {
+					GameObject.Find ("GameController").GetComponent<GameController> ().finishedAnimation ();
+					isAnimating = false;
+				} else if (isPlayerClicking) {
+					GameObject.Find ("GameController").GetComponent<GameController> ().verifyClick (gameObject);
+					isPlayerClicking = false;
+				}
+			}
+		}
+	}
+
+	public void animatePiece() {
+		//Trigger animation here
+		isAnimating = true;
+		animate.Play ("Flicker");
+
+	}
+
+	void OnMouseDown() {
+		if (GameObject.Find ("GameController").GetComponent<GameController> ().getPlayerPhase ()) {
+			isPlayerClicking = true;
+			animate.Play ("Flicker");
+		}
+
+	}
+}
