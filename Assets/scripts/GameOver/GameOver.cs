48094f7896922751a7146770025e6c74b0f165e0
caseyboy65 <caseyboy65@yahoo.com>
2015-10-15 08:42:09 -0400

    Updated game over screen
    Made game over screen look similar to main menu screen.

--- Assets/scripts/GameOver/GameOver.cs
+++ Assets/scripts/GameOver/GameOver.cs
@@ -3,8 +3,18 @@ using System.Collections;
 
 public class GameOver : MonoBehaviour {
 
+	public float hoverOverSizeIncrease;
+
 	void OnMouseDown() {
 		Debug.Log ("Going back to main menu");
 		Application.LoadLevel ("MainMenu");
 	}
+	void OnMouseEnter() {
+		gameObject.transform.localScale += new Vector3(hoverOverSizeIncrease,hoverOverSizeIncrease,0);
+	}
+	
+	void OnMouseExit() {
+		gameObject.transform.localScale -= new Vector3(hoverOverSizeIncrease,hoverOverSizeIncrease,0);
+	}
+
 }
